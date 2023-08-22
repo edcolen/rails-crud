4e49e7dc154e0f535d92e361aff663e586c5cc35
Ed Colen <ed.colen@gmail.com>
2023-08-22 11:50:43 -0300

    class complete

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,4 @@
 class Restaurant < ApplicationRecord
+  validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
+  validates :name, presence: true
 end
